8cbad02b2d17947a36c739522d81274202dab510
Hamid Reza <arzaghi@outlook.com>
2020-07-26 20:27:08 +0430

    some minor refactoring

--- sparse.hpp
+++ sparse.hpp
@@ -38,13 +38,13 @@ public:
         Iterator() : sparse_{ nullptr }, index_{ 0 } {}
         Iterator(Sparse<T>& sparse, size_t index) : sparse_{ &sparse }, index_{ index } {}             
 
-        Iterator& operator=(const Iterator& rawIterator) { index_ = rawIterator.index_; return *this; }
+        Iterator& operator=(const Iterator& iter) { index_ = iter.index_; return *this; }
         Iterator& operator=(const difference_type& index) { index_ = index; return *this; }
 
         operator const difference_type& () const { return index_; }
-        bool operator==(const Iterator& rawIterator) const { return (index_ == rawIterator.index_ && sparse_->values_.begin() == rawIterator.sparse_->values_.begin()); }
+        bool operator==(const Iterator& iter) const { return (index_ == iter.index_ && sparse_->values_.begin() == iter.sparse_->values_.begin()); }
         bool operator==(const difference_type& index) const { return (index_ == index); }
-        bool operator!=(const Iterator& rawIterator) const { return !((*this) == rawIterator); }
+        bool operator!=(const Iterator& iter) const { return !((*this) == iter); }
         bool operator!=(const difference_type& index) const { return (index_ != index); }
 
         Iterator& operator+=(const difference_type& movement) { index_ += movement; return (*this); }
@@ -56,25 +56,25 @@ public:
         Iterator& operator--() { --index_; return (*this); }        
         Iterator  operator--(int) { auto temp(*this); --index_; return temp; }        
 
-        Iterator  operator-(const Iterator& rawIterator) const { return  Iterator(*sparse_, index_ - rawIterator.index_); }
+        Iterator  operator-(const Iterator& iter) const { return  Iterator(*sparse_, index_ - iter.index_); }
         Iterator  operator-(const difference_type& index) const { return Iterator(*sparse_, index_ - index); }
         Iterator  operator-(const int& index) const { return Iterator(*sparse_, index_ - index); }
 
-        Iterator  operator+(const Iterator& rawIterator) const { return  Iterator(*sparse_, index_ + rawIterator.index_); }
+        Iterator  operator+(const Iterator& iter) const { return  Iterator(*sparse_, index_ + iter.index_); }
         Iterator  operator+(const difference_type& index) const { return Iterator(*sparse_, index_ + index); }
         Iterator  operator+(const int& index) const { return Iterator(*sparse_, index_ + index); }
 
         proxy operator*() { return ((*sparse_)[index_]); }
         proxy* operator->() { return &((*sparse_)[index_]); }
 
-        inline bool operator<(const Iterator& rawIterator) const { return index_ < rawIterator.index_; }
-        inline bool operator<=(const Iterator& rawIterator) const { return index_ <= rawIterator.index_; }        
-        inline bool operator>(const Iterator& rawIterator) const { return index_ > rawIterator.index_; }        
-        inline bool operator>=(const Iterator& rawIterator) const { return index_ >= rawIterator.index_; }
+        inline bool operator<(const Iterator& iter) const { return index_ < iter.index_; }
+        inline bool operator<=(const Iterator& iter) const { return index_ <= iter.index_; }        
+        inline bool operator>(const Iterator& iter) const { return index_ > iter.index_; }        
+        inline bool operator>=(const Iterator& iter) const { return index_ >= iter.index_; }
 
     protected:
         Sparse<T>* sparse_;
-        size_t      index_;
+        size_t     index_;
     };
 
     class Const_Iterator  {
@@ -89,13 +89,13 @@ public:
         Const_Iterator(const Sparse<T>& sparse, size_t index) : sparse_{ &sparse }, index_{ index } {}
         Const_Iterator() : sparse_{ nullptr }, index_{ 0 } {}
 
-        Const_Iterator& operator=(const Const_Iterator& rawIterator)    { index_ = rawIterator.index_; return *this; }
+        Const_Iterator& operator=(const Const_Iterator& iter)    { index_ = iter.index_; return *this; }
         Const_Iterator& operator=(const difference_type& index)         { index_ = index; return *this; }
 
         operator const difference_type& () const { return index_; }        
-        bool operator==(const Const_Iterator& rawIterator)  const { return (index_ == rawIterator.index_ && sparse_->values_.begin() == rawIterator.sparse_->values_.begin()); }
+        bool operator==(const Const_Iterator& iter)  const { return (index_ == iter.index_ && sparse_->values_.begin() == iter.sparse_->values_.begin()); }
         bool operator==(const difference_type& index)       const { return (index_ == index); }
-        bool operator!=(const Const_Iterator& rawIterator)  const { return !((*this) == rawIterator); }
+        bool operator!=(const Const_Iterator& iter)  const { return !((*this) == iter); }
         bool operator!=(const difference_type& index)       const { return (index_ != index); }
 
 
@@ -108,11 +108,11 @@ public:
         Const_Iterator& operator--()    { --index_; return (*this); }        
         Const_Iterator  operator--(int) { auto temp(*this); --index_; return temp; }        
 
-        Const_Iterator  operator-(const Const_Iterator& rawIterator) const { return  Const_Iterator(*sparse_, index_ - rawIterator.index_); }
+        Const_Iterator  operator-(const Const_Iterator& iter) const { return  Const_Iterator(*sparse_, index_ - iter.index_); }
         Const_Iterator  operator-(const difference_type& index) const { return Const_Iterator(*sparse_, index_ - index); }
         Const_Iterator  operator-(const int& index) const { return Const_Iterator(*sparse_, index_ - index); }
 
-        Const_Iterator  operator+(const Const_Iterator& rawIterator) const { return  Const_Iterator(*sparse_, index_ + rawIterator.index_); }
+        Const_Iterator  operator+(const Const_Iterator& iter) const { return  Const_Iterator(*sparse_, index_ + iter.index_); }
         Const_Iterator  operator+(const difference_type& index) const { return Const_Iterator(*sparse_, index_ + index); }
         Const_Iterator  operator+(const int& index) const { return Const_Iterator(*sparse_, index_ + index); }
                 
@@ -136,13 +136,13 @@ public:
         Reverse_Iterator() : Iterator() {}
         Reverse_Iterator(Sparse<T>& sparse, size_t index) : Iterator(sparse, index) {}                
                 
-        operator const difference_type& ()                      const { return Iterator::index_; }
-        bool operator==(const Reverse_Iterator& rawIterator)    const { return (Iterator::index_ == rawIterator.index_ && Iterator::sparse_->values_.begin() == rawIterator.sparse_->values_.begin()); }
-        bool operator==(const difference_type& index)           const { return (Iterator::index_ == index); }
-        bool operator!=(const Reverse_Iterator& rawIterator)    const { return !((*this) == rawIterator); }
-        bool operator!=(const difference_type& index)           const { return (Iterator::index_ != index); }
+        operator const difference_type& ()               const { return Iterator::index_; }
+        bool operator==(const Reverse_Iterator& iter)    const { return (Iterator::index_ == iter.index_ && Iterator::sparse_->values_.begin() == iter.sparse_->values_.begin()); }
+        bool operator==(const difference_type& index)    const { return (Iterator::index_ == index); }
+        bool operator!=(const Reverse_Iterator& iter)    const { return !((*this) == iter); }
+        bool operator!=(const difference_type& index)    const { return (Iterator::index_ != index); }
 
-        Reverse_Iterator& operator=(const Reverse_Iterator& rawIterator) { Iterator::index_ = rawIterator.index_; return *this; }
+        Reverse_Iterator& operator=(const Reverse_Iterator& iter) { Iterator::index_ = iter.index_; return *this; }
         Reverse_Iterator& operator=(const difference_type& index) { Iterator::index_ = index; return *this; }        
         
         Reverse_Iterator& operator+=(const difference_type& movement) { Iterator::index_ -= movement; return (*this); }
@@ -154,12 +154,12 @@ public:
         Reverse_Iterator& operator--() { ++Iterator::index_; return (*this); }        
         Reverse_Iterator  operator--(int) { auto temp(*this); ++Iterator::index_; return temp; }   
 
-        Reverse_Iterator  operator-(const Reverse_Iterator& rawIterator)    const   { return Reverse_Iterator(*Iterator::sparse_, Iterator::index_ - rawIterator.index_); }
-        Reverse_Iterator  operator-(const difference_type& index)           const   { return Reverse_Iterator(*Iterator::sparse_, Iterator::index_ - index); }
-        Reverse_Iterator  operator-(const int& index)                       const   { return Reverse_Iterator(*Iterator::sparse_, Iterator::index_ - index); }        
-        Reverse_Iterator  operator+(const Reverse_Iterator& rawIterator)    const   { return Reverse_Iterator(*Iterator::sparse_, Iterator::index_ + rawIterator.index_); }
-        Reverse_Iterator  operator+(const difference_type& index)           const   { return Reverse_Iterator(*Iterator::sparse_, Iterator::index_ + index); }
-        Reverse_Iterator  operator+(const int& index)                       const   { return Reverse_Iterator(*Iterator::sparse_, Iterator::index_ + index); }
+        Reverse_Iterator  operator-(const Reverse_Iterator& iter)       const   { return Reverse_Iterator(*Iterator::sparse_, Iterator::index_ - iter.index_); }
+        Reverse_Iterator  operator-(const difference_type& index)       const   { return Reverse_Iterator(*Iterator::sparse_, Iterator::index_ - index); }
+        Reverse_Iterator  operator-(const int& index)                   const   { return Reverse_Iterator(*Iterator::sparse_, Iterator::index_ - index); }        
+        Reverse_Iterator  operator+(const Reverse_Iterator& iter)       const   { return Reverse_Iterator(*Iterator::sparse_, Iterator::index_ + iter.index_); }
+        Reverse_Iterator  operator+(const difference_type& index)       const   { return Reverse_Iterator(*Iterator::sparse_, Iterator::index_ + index); }
+        Reverse_Iterator  operator+(const int& index)                   const   { return Reverse_Iterator(*Iterator::sparse_, Iterator::index_ + index); }
     };
 
     class Const_Reverse_Iterator : public Const_Iterator {
@@ -174,12 +174,12 @@ public:
         Const_Reverse_Iterator(const Sparse<T>& sparse, size_t index) :  Const_Iterator(sparse, index){}        
         
         operator const difference_type& () const { return Const_Iterator::index_; }
-        bool operator==(const Const_Reverse_Iterator& rawIterator)  const { return (Const_Iterator::index_ == rawIterator.index_ && Const_Iterator::sparse_->values_.begin() == rawIterator.sparse_->values_.begin()); }
+        bool operator==(const Const_Reverse_Iterator& iter)  const { return (Const_Iterator::index_ == iter.index_ && Const_Iterator::sparse_->values_.begin() == iter.sparse_->values_.begin()); }
         bool operator==(const difference_type& index)       const { return (Const_Iterator::index_ == index); }
-        bool operator!=(const Const_Reverse_Iterator& rawIterator)  const { return !((*this) == rawIterator); }
+        bool operator!=(const Const_Reverse_Iterator& iter)  const { return !((*this) == iter); }
         bool operator!=(const difference_type& index)       const { return (Const_Iterator::index_ != index); }
 
-        Const_Reverse_Iterator& operator=(const Const_Reverse_Iterator& rawIterator) { Const_Iterator::index_ = rawIterator.index_; return *this; }
+        Const_Reverse_Iterator& operator=(const Const_Reverse_Iterator& iter) { Const_Iterator::index_ = iter.index_; return *this; }
         Const_Reverse_Iterator& operator=(const difference_type& index) { Const_Iterator::index_ = index; return *this; }
 
         Const_Reverse_Iterator& operator+=(const ptrdiff_t& movement) { Const_Iterator::index_ -= movement; return (*this); }
@@ -191,11 +191,11 @@ public:
         Const_Reverse_Iterator& operator--() { ++Const_Iterator::index_; return (*this); }        
         Const_Reverse_Iterator  operator--(int) { auto temp(*this); ++Const_Iterator::index_; return temp; }
 
-        Const_Reverse_Iterator  operator-(const Const_Reverse_Iterator& rawIterator) const { return  Const_Reverse_Iterator(*Const_Iterator::sparse_, Const_Iterator::index_ - rawIterator.index_); }
+        Const_Reverse_Iterator  operator-(const Const_Reverse_Iterator& iter) const { return  Const_Reverse_Iterator(*Const_Iterator::sparse_, Const_Iterator::index_ - iter.index_); }
         Const_Reverse_Iterator  operator-(const difference_type& index) const { return Const_Reverse_Iterator(*Const_Iterator::sparse_, Const_Iterator::index_ - index); }
         Const_Reverse_Iterator  operator-(const int& index) const { return Const_Reverse_Iterator(*Const_Iterator::sparse_, Const_Iterator::index_ - index); }
 
-        Const_Reverse_Iterator  operator+(const Const_Reverse_Iterator& rawIterator) const { return  Const_Reverse_Iterator(*Const_Iterator::sparse_, Const_Iterator::index_ + rawIterator.index_); }
+        Const_Reverse_Iterator  operator+(const Const_Reverse_Iterator& iter) const { return  Const_Reverse_Iterator(*Const_Iterator::sparse_, Const_Iterator::index_ + iter.index_); }
         Const_Reverse_Iterator  operator+(const difference_type& index) const { return Const_Reverse_Iterator(*Const_Iterator::sparse_, Const_Iterator::index_ + index); }
         Const_Reverse_Iterator  operator+(const int& index) const { return Const_Reverse_Iterator(*Const_Iterator::sparse_, Const_Iterator::index_ + index); }
     };
@@ -246,7 +246,7 @@ public:
     inline iterator                 end()               { return iterator(*this, count_); }
     inline const_iterator           begin()     const   { return cbegin(); }
     inline const_iterator           end()       const   { return cend(); }
-    inline const_iterator           cbegin()    const   { return const_iterator((*this), 0); }
+    inline const_iterator           cbegin()    const   { return const_iterator(*this, 0); }
     inline const_iterator           cend()      const   { return const_iterator(*this, count_); }
 
     inline reverse_iterator         rbegin()            { return reverse_iterator(*this, count_ - 1); }
@@ -263,11 +263,3 @@ private:
     size_t              count_;
     T                   sparse_default_value_;
 };
-
-
-template <typename T> inline static bool operator<(const typename Sparse<T>::iterator& lhs, const typename Sparse<T>::iterator& rhs) { return true; }
-template <typename T> inline static bool operator<(const typename Sparse<T>::const_iterator& lhs, const typename Sparse<T>::const_iterator& rhs) { return true; }
-//template <typename T> inline static bool operator<(const typename Sparse<T>::reverse_iterator& lhs, const typename Sparse<T>::reverse_iterator& rhs) { return true; }
-//size_t  operator-(const typename Sparse<int>::Iterator& a, const typename Sparse<int>::Iterator& b) { return a.index_ - b.index_; }
-
-//Sparse<int>::Iterator  operator+(const typename Sparse<int>::Iterator& a, const typename Sparse<int>::Iterator& b) { return a + b; }
